453894e7f734d3e877f2e70b7bc11f0ddf961833
task4233 <tks.m1205@gmail.com>
2019-09-21 09:24:31 +0900

    [UPDATE] main.cpp for test

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@ extern "C" {
     int fib(int);
 }
 
-int main() {
-    std::cout << "Call fib with 10: " << fib(10) << std::endl;
+extern "C" {
+  int myfunc(int,int);
+}
 
+int main() {
+  // std::cout << "Call fib with 10: " << fib(10) << std::endl;
+  std::cout << "test myfunc(5,10)" << myfunc(5,10) << std::endl;
     return 0;
 }
