c8ce3f0740680f26b9328394cbf8d6cb3bb466a6
task4233 <tks.m1205@gmail.com>
2019-09-21 10:52:41 +0900

    [REMOVE] unnecessary parts

--- main.cpp
+++ main.cpp
@@ -4,12 +4,7 @@ extern "C" {
     int fib(int);
 }
 
-extern "C" {
-  int myfunc(int,int);
-}
-
 int main() {
-  // std::cout << "Call fib with 10: " << fib(10) << std::endl;
-  std::cout << "test myfunc(5,10)" << myfunc(5,10) << std::endl;
-    return 0;
+  std::cout << "Call fib with 10: " << fib(10) << std::endl;
+  return 0;
 }
